dd1b0c968f0086a9f5e33a8587cd58bb545042a9
adlingtb <adlingtb@gmail.com>
2022-04-29 22:44:26 +0100

    Dealer finished

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,4 +1,5 @@
 let Cards = [];
+let dealerTotal = 0;
 
 function Card(val, img, isAce) {
     this.val = val;
@@ -12,28 +13,35 @@ function Card(val, img, isAce) {
 
 function newGame(){
     createObj();
+    while(dealerTotal < 17){
+        dealDealer();
+    }
+    if(dealerTotal === 21){
+        document.getElementById("dealerBlackjackBanner").style.display = 'block'
+        console.log("blackjack")
+    }
+    if(dealerTotal >21){
+        document.getElementById("dealerBustBanner").style.display = 'block'
+        console.log("bust")
+    }
+
+
+
+}
+
+function dealDealer(){
     let pick = Math.floor(Math.random() * Cards.length);
     console.log("pick", pick);
     dealCardDealer(Cards[pick].img);
-    pick = Math.floor(Math.random() * Cards.length);
-    console.log("pick", pick);
-    dealCardDealer(Cards[pick].img);
-    pick = Math.floor(Math.random() * Cards.length);
-    console.log("pick", pick);
-    dealCardDealer(Cards[pick].img);
-    pick = Math.floor(Math.random() * Cards.length);
-    console.log("pick", pick);
-    dealCardDealer(Cards[pick].img);
-    pick = Math.floor(Math.random() * Cards.length);
-    console.log("pick", pick);
-    dealCardDealer(Cards[pick].img);
+    dealerTotal += Cards[pick].val;
+    document.getElementById("dealerTotal").innerText = dealerTotal;
 }
 
 function dealCardDealer(img){
-    let elem = document.createElement("img");
-    elem.setAttribute("src", img);
-    elem.setAttribute("width", "20%");
-    elem.setAttribute("style", "margin: 10px");
+    let el = document.createElement("img");
+    el.setAttribute("src", img);
+    el.setAttribute("width", "20%");
+    el.setAttribute("style", "margin: 10px");
 
-    document.getElementById("dealerCards").appendChild(elem);
+    document.getElementById("dealerCards").appendChild(el);
 }
