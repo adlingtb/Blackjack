d99f062bf1226e02defa40edb94cdeb97e01b528
adlingtb <adlingtb@gmail.com>
2022-04-30 02:40:36 +0100

    Player functionality added

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,5 +1,6 @@
 let Cards = [];
 let dealerTotal = 0;
+let playerTotal = 0;
 
 function Card(val, img, isAce) {
     this.val = val;
@@ -11,10 +12,15 @@ function Card(val, img, isAce) {
 
 function resetGame(){
     document.getElementById("dealerCards").innerHTML = "";
+
     dealerTotal = 0;
     document.getElementById("dealerBlackjackBanner").style.display = 'none';
     document.getElementById("dealerBustBanner").style.display = 'none';
 
+    document.getElementById("playerCards").innerHTML = "";
+    playerTotal = 0;
+    document.getElementById("playerBlackjackBanner").style.display = 'none';
+    document.getElementById("playerBustBanner").style.display = 'none';
 }
 
 function dealerDefault(){
@@ -26,6 +32,22 @@ function newGame(){
     createObj();
     resetGame();
 
+
+}
+
+function hit(){
+    dealPlayer();
+    if(playerTotal === 21){
+        document.getElementById("playerBlackjackBanner").style.display = 'block'
+        console.log("blackjack")
+    }
+    if(playerTotal >21){
+        document.getElementById("playerBustBanner").style.display = 'block'
+        console.log("bust")
+    }
+}
+function stand(){
+    document.getElementById("dealerCards").innerHTML = "";
     while(dealerTotal < 17){
         dealDealer();
     }
@@ -37,14 +59,10 @@ function newGame(){
         document.getElementById("dealerBustBanner").style.display = 'block'
         console.log("bust")
     }
-
-
-
 }
 
 function dealDealer(){
     let pick = Math.floor(Math.random() * Cards.length);
-    console.log("pick", pick);
     dealCardDealer(Cards[pick].img);
     dealerTotal += Cards[pick].val;
     document.getElementById("dealerTotal").innerText = dealerTotal;
@@ -58,3 +76,21 @@ function dealCardDealer(img){
 
     document.getElementById("dealerCards").appendChild(el);
 }
+
+
+function dealPlayer(){
+    let pick = Math.floor(Math.random() * Cards.length);
+    dealCardPlayer(Cards[pick].img);
+    playerTotal += Cards[pick].val;
+    document.getElementById("playerTotal").innerText = playerTotal;
+}
+
+function dealCardPlayer(img){
+    let el = document.createElement("img");
+    el.setAttribute("src", img);
+    el.setAttribute("width", "20%");
+    el.setAttribute("style", "margin: 10px");
+
+    document.getElementById("playerCards").appendChild(el);
+}
+
